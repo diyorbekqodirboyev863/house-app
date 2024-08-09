062920ed124fcc5260b7d363a36b18be20126731
Diyorbek Qodirboyev <diyorbekqodirboyev863@gmail.com>
2024-08-09 08:57:36 +0500

    Several houses have been added.

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import House
 
 # Home.
 def home(request):
-    return render(request=request, template_name='home.html', context={})
+    houses = House.objects.all()
+    return render(request=request, template_name='home.html', context={'houses': houses})
